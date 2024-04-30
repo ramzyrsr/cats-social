305b8fb01fc8732f57520203f3314c25a57f2e1f
Ramzy Syahrul R <ramzy.sramadhan@gmail.com>
2024-04-30 10:57:45 +0700

    init router

---
+++ router/router.go
@@ -0,0 +1,17 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Cats Social",
+		})
+	})
+
+	return router
+}
