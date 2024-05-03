f0e5396736d94b580db2779531544072ef067e1a
Ramzy Syahrul R <ramzy.sramadhan@gmail.com>
2024-05-03 10:04:15 +0700

    remove db config

--- main.go
+++ main.go
@@ -2,38 +2,11 @@ package main
 
 import (
 	"cats-social/router"
-	"database/sql"
-	"fmt"
-	"log"
-	"os"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Error loading .env file:", err)
-		return
-	}
-
-	// Reading environment variables
-	dbName := os.Getenv("DB_NAME")
-	dbPort := os.Getenv("DB_PORT")
-	dbHost := os.Getenv("DB_HOST")
-	dbUsername := os.Getenv("DB_USERNAME")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbParams := os.Getenv("DB_PARAMS")
-
-	connString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?%s", dbUsername, dbPassword, dbHost, dbPort, dbName, dbParams)
-
-	db, err := sql.Open("pgx", connString)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
 	// Setup routes
 	r := router.SetupRouter()
 
